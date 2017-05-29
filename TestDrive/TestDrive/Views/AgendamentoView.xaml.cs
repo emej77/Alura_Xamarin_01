a5933060b1a96e2b5f358be5c91fe806d0f066b4
Elias Mansur Espindola Junior <emej77@gmail.com>
2017-05-29 15:06:47 -0300

    FinalizandoCurso
    Acabei o primeiro curso de Xamarin da Alura.com.br

--- TestDrive/TestDrive/Views/AgendamentoView.xaml.cs
+++ TestDrive/TestDrive/Views/AgendamentoView.xaml.cs
@@ -14,12 +14,36 @@ namespace TestDrive.Views
     {
         public Veiculo veiculo { get; set; }
 
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+
+        DateTime dataAgendamento = DateTime.Today;
+        public DateTime DataAgendamento
+        {
+            get
+            {
+                return dataAgendamento;
+            }
+            set
+            {
+                dataAgendamento = value;
+            }
+        }
+
+        public TimeSpan HoraAgendamento { get; set; }
+
         public AgendamentoView(Veiculo veiculo)
         {
             InitializeComponent();
             this.veiculo = veiculo;
 
             this.BindingContext = this;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Agendamento", string.Format("Nome: {0}\nTelefone: {1}\nE-mail: {2}\nData Agendamento: {3}\nHora Agendamento: {4}",Nome, Telefone, Email, DataAgendamento.ToString("dd/MM/yyyy"), HoraAgendamento), "Agendar");
         }
     }
 }
